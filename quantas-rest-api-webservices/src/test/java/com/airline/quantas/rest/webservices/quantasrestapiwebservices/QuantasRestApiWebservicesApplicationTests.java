adcf369e3258225aa1a04bce9f84f84bf318d37f
sahasouravcse <35940223+sahasouravcse@users.noreply.github.com>
2018-05-25 01:02:20 +1000

    Update QuantasRestApiWebservicesApplicationTests.java

--- quantas-rest-api-webservices/src/test/java/com/airline/quantas/rest/webservices/quantasrestapiwebservices/QuantasRestApiWebservicesApplicationTests.java
+++ quantas-rest-api-webservices/src/test/java/com/airline/quantas/rest/webservices/quantasrestapiwebservices/QuantasRestApiWebservicesApplicationTests.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
-public class QuantasRestApiWebservicesApplicationSpringBootTests2 {
+public class QuantasRestApiWebservicesApplicationTests {
 	
 	@Autowired
 	private TestRestTemplate restTemplate;
